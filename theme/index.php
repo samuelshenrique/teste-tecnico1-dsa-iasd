92a07c8aa8e5cd2c91430a3459294488f588b50c
Samuel Santos Henrique <samuel.santos99@hotmail.com>
2023-11-22 19:49:08 -0300

    feature:Implementação da primeira etapa do teste técnico

---
+++ theme/index.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+
+?>
+
+<?php
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+?>
+<div id="post-<?php the_ID(); ?>" <?php post_class(['container', 'conteudo__principal']); ?>>
+<?php the_content(); ?>
+</div>
+<?php
+    endwhile;
+else :
+    _e('Desculpe, não temos posts nestas condições');
+endif;
+?>
+
+<?
+get_footer();
